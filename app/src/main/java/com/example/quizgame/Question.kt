46f0384b7aae0c7d8c6cb4be47d0414937b1837e
PatKowal <pk310643@student.polsl.pl>
2026-06-07 16:09:30 +0200

    feat: Question changed

--- app/src/main/java/com/example/quizgame/Question.kt
+++ app/src/main/java/com/example/quizgame/Question.kt
@@ -1,11 +1,11 @@
 package com.example.quizgame
 
-class Question {
-    val question: String
-    val correctAnswer: String
-
-    constructor(question: String, correctAnswer: String) {
-        this.question = question
-        this.correctAnswer = correctAnswer
-    }
+data class Question(private val question: String, private val correctAnswer: String) {
+//    val question: String
+//    val correctAnswer: String
+//
+//    constructor(question: String, correctAnswer: String) {
+//        this.question = question
+//        this.correctAnswer = correctAnswer
+//    }
 }
