b8308e244a7155a02861d8fd92289594dd707262
JakubGuzik22 <jg310578@student.polsl.pl>
2026-06-07 16:01:44 +0200

    Question class

--- app/src/main/java/com/example/quizgame/Question.kt
+++ app/src/main/java/com/example/quizgame/Question.kt
@@ -1,4 +1,11 @@
 package com.example.quizgame
 
 class Question {
+    val question: String
+    val correctAnswer: String
+
+    constructor(question: String, correctAnswer: String) {
+        this.question = question
+        this.correctAnswer = correctAnswer
+    }
 }
