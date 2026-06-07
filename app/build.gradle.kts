a686e447d74c2d94d0b87449c2a3db8a360baf95
JakubGuzik22 <jg310578@student.polsl.pl>
2026-06-07 15:43:41 +0200

    QuizActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.quizgame"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.quizgame"
         minSdk = 33
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
